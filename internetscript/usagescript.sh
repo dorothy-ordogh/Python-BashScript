7ee54b129944fbe7f5ee4b23667dbcbc59c416fb
dordogh <dorothyordogh@dorothy-ordoghs-macbook-pro.local>
2014-10-20 18:19:51 -0700

    another fix to format

--- internetscript/usagescript.sh
+++ internetscript/usagescript.sh
@@ -2,8 +2,5 @@
 
 curl http://ubcit.webi.it.ubc.ca/__shared/Pagelet5764.html -o "usage.txt"
 
-<<<<<<< Updated upstream
-python /Users/[user]/Desktop/internetscript/readusage.py
-=======
 python /Users/dorothyordogh/Python-BashScript/internetscript/readusage.py
->>>>>>> Stashed changes
+
