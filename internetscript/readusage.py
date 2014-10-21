fb2a4922ab0ecdabab1224b892681a34e18c5e65
dorothy-ordogh <dorothy-ordogh@hotmail.com>
2014-10-20 18:23:59 -0700

    Update readusage.py

--- internetscript/readusage.py
+++ internetscript/readusage.py
@@ -8,10 +8,8 @@ def main():
 			if "Usage (Last 24 hours)" in line:
 				iend = line.find('G inbound')
 				istart = iend -4
-				print(istart)
 				numstring = line[istart:iend]
 				num = float(numstring)
-				print(num)
 				if num >= 6.70:
 					cmd = """osascript -e 'tell app "Finder" to display dialog "If you watch any more Netflix, you are going to be put in INTERNET JAIL!!"'"""
 					os.system(cmd)
