ed5a2dd08eac61a2cab68ad078830d40a99d5289
dorothy-ordogh <dorothy-ordogh@hotmail.com>
2014-12-27 12:50:05 -0800

    Update readusage.py

--- internetscript/readusage.py
+++ internetscript/readusage.py
@@ -1,16 +1,20 @@
 #!/Users/[user]/anaconda/bin/python
-
+#Import os so a finder pop up can notify me
 import os
 
 def main():
-	with open('/Users/[user]/usage.txt') as f:
+	#Open file that contains usage levels
+	with open('~/usage.txt') as f:
 		for line in f:
+			#If the line contains the usage level, then parse number and
+			#compare to preset number (threshold)
 			if "Usage (Last 24 hours)" in line:
 				iend = line.find('G inbound')
 				istart = iend -4
 				numstring = line[istart:iend]
 				num = float(numstring)
 				if num >= 6.70:
+					#Finder pop up to notify me of my internet usage with a funny message
 					cmd = """osascript -e 'tell app "Finder" to display dialog "If you watch any more Netflix, you are going to be put in INTERNET JAIL!!"'"""
 					os.system(cmd)
 				else:
