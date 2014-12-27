91ea1ec0cb99128eb1f9f47d88bff35819f82c30
dorothy-ordogh <dorothy-ordogh@hotmail.com>
2014-12-27 12:51:01 -0800

    Update usagescript.sh

--- internetscript/usagescript.sh
+++ internetscript/usagescript.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+#Download html to a file
 curl http://ubcit.webi.it.ubc.ca/__shared/Pagelet5764.html -o "usage.txt"
 
+#Run python script
 python /Users/[user]/Python-BashScript/internetscript/readusage.py
 
